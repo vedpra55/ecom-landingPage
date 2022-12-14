10317171406779de4d0b15524fc4023717753e82
vedpra55 <vedna400@gmail.com>
2022-12-14 17:55:14 +0530

    changes

--- pages/all-brands/index.js
+++ pages/all-brands/index.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavBar, Footer, Breadcrumbs } from "../../components";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
 export default function AllBrandsPage() {
+  const [itemsOpen, setItemsOpen] = useState(["0"]);
+
+  function handleOpen(i) {
+    const list = itemsOpen;
+    const newList = list.push(i.toString());
+    console.log(newList);
+    setItemsOpen([newList]);
+  }
+
   return (
     <main>
       <NavBar />
@@ -11,24 +20,55 @@ export default function AllBrandsPage() {
         <Breadcrumbs text={"Хлебные крошки"} />
         <div className="mt-[30px]">
           <h1 className="font-bold text-3xl">Все бренды</h1>
-          <div className="mt-5 grid grid-cols-12 gap-x-5 flex-wrap md:gap-x-[40px] gap-y-5 items-center">
+          <div className="mt-5  grid grid-cols-12 gap-x-5 flex-wrap md:gap-x-[40px] gap-y-5 items-start">
             {Array.from(Array(12)).map((item, i) => (
-              <div
-                key={i}
-                className=" cardHover cursor-pointer col-span-12 md:col-span-6 px-5 md:px-[30px] h-[60px] w-full 3xl:w-[580px] flex justify-between items-center myshadow bg-white rounded-[5px]"
-              >
-                <p className="font-medium md:text-[18px]">
-                  Часто задаваемые вопросы ?
-                </p>
-                <div className="bg-main btnHover cursor-pointer rounded-full  w-[24px] h-[24px] flex justify-center items-center">
-                  <AiOutlinePlus className="text-xl text-white" />
-                </div>
-              </div>
+              <Card key={i} />
             ))}
           </div>
         </div>
       </section>
       <Footer />
     </main>
+  );
+}
+
+function Card() {
+  const [isOpen, setOpen] = useState(false);
+  return (
+    <div className="col-span-12  flex flex-col md:col-span-6">
+      <div className=" cardHover cursor-pointer  px-5 md:px-[30px] h-[60px] w-full 3xl:w-[580px] flex justify-between items-center myshadow bg-white rounded-[5px]">
+        <p className="font-medium md:text-[18px]">Часто задаваемые вопросы ?</p>
+
+        {isOpen ? (
+          <div className="bg-main btnHover cursor-pointer rounded-full  w-[24px] h-[24px] flex justify-center items-center">
+            <AiOutlineMinus
+              onClick={() => setOpen(false)}
+              className="text-xl text-white"
+            />
+          </div>
+        ) : (
+          <div className="bg-main btnHover cursor-pointer rounded-full  w-[24px] h-[24px] flex justify-center items-center">
+            <AiOutlinePlus
+              onClick={() => setOpen(true)}
+              className="text-xl text-white"
+            />
+          </div>
+        )}
+      </div>
+
+      {isOpen && (
+        <div className="w-full mb-12 h-56 px-5 py-5 flex gap-y-1 flex-col">
+          <p>Random item</p>
+          <p>Random item</p>
+          <p>Random item</p>
+          <p>Random item</p>
+          <p>Random item</p>
+          <p>Random item</p>
+          <p>Random item</p>
+          <p>Random item</p>
+          <p>Random item</p>
+        </div>
+      )}
+    </div>
   );
 }
